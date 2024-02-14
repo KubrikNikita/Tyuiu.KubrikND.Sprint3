1200dee1b397fa77138807b18ed0e9fb0b6a71bc
Tyuiu.KubrikND@outlook.com <Tyuiu.KubrikND@outlook.com>
2024-02-14 21:23:02 +0500

    Task6(v2)

---
+++ Tyuiu.KubrikND.Sprint3.Task6.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KubrikND.Sprint3.Task6.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KubrikND.Sprint3.Task6.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d181a57-cb66-405c-8540-bbaf18663ab2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KubrikND.Sprint3.Task6.V2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KubrikND.Sprint3.Task6.V2.Lib;
+
+namespace Tyuiu.KubrikND.Sprint3.Task6.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Кубрик Н.Д.| ИСПБ-23-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine(" Спринт #3                                                               ");
+            Console.WriteLine(" Тема: Обработка целочисленной информации                                ");
+            Console.WriteLine(" Задание #6                                                              ");
+            Console.WriteLine(" Вариант 2                                                               ");
+            Console.WriteLine(" Выполнил: Кубрик Н.Д.| ИСПБ-23-1                                        ");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               ");
+            Console.WriteLine("Напишите программу, которая ищет среди целых чисел, принадлежащих        ");
+            Console.WriteLine("числовому отрезку [12, 18] количество всех делителей                     ");
+            Console.WriteLine("                                                                         ");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                       ");
+            Console.WriteLine("*************************************************************************");
+            int startValue = 12;
+            int stopValue = 18;
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("*Результат                                                              *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("Сумма = " + ds.GetSumTheDivisors(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
